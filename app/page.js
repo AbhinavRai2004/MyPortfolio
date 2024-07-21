73da0a40dc3be9b7d3441a351a7581149fb5b50d
Abhinav <raiabhinav.in@gmail.com>
2024-07-21 18:41:32 +0530

    page & layout file updated

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-"use client";
-
-import { useState } from "react";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -8,56 +5,15 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
-
-  const lightModeStyles = {
-    backgroundColor: 'white',
-    color: 'black',
-  };
-  
-  const darkModeStyles = {
-    backgroundColor: 'black',
-    color: 'white',
-  };
-
-  const commonStyles = {
-    minHeight: '100vh',
-    margin: 0,
-    padding: 0,
-    transition: 'background-color 0.3s, color 0.3s',
-  };
-
-  const appliedStyles = darkMode ? darkModeStyles : lightModeStyles;
-
+export default async function Home() {
   return (
-    <div style={{ ...commonStyles, ...appliedStyles }}>
-      <button 
-        onClick={toggleDarkMode} 
-        style={{ 
-          position: 'fixed', 
-          top: '10px', 
-          right: '10px',
-          padding: '10px',
-          cursor: 'pointer',
-          backgroundColor: darkMode ? 'white' : 'black',
-          color: darkMode ? 'black' : 'white',
-          border: 'none',
-          borderRadius: '5px'
-        }}
-      >
-        {darkMode ? 'Light Mode' : 'Dark Mode'}
-      </button>
+    <>
       <HeroSection />
       <AboutSection />
       <Skills />
       <Projects />
       <Education />
       <ContactSection />
-    </div>
-  );
-}
+    </>
+  )
+};
