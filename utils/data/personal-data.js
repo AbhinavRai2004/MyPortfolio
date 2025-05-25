cd8f70959d13012972a8ed1b58eb8b9b640ed606
Abhinav <raiabhinav.in@gmail.com>
2025-05-25 07:23:17 +0530

    resume updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,14 +1,16 @@
 export const personalData = {
   name: "Abhinav Rai",
-  profile: '/profile.png',
-  description: "My name is Abhinav Rai, and I am a dedicated and enthusiastic programmer. I am a quick learner with a strong self-learning attitude, always eager to explore and master new technologies. I have a passion for problem-solving and a deep interest in all aspects of web application development. My core skill lies in ReactJs, NodeJs, JavaScript, and I enjoy leveraging it to create open and accessible web solutions. I am open to any job opportunities that align with my skills and interests.",
-  email: 'raiabhinav.in@gmail.com',
-  phone: '+91-9335783068',
-  address: 'Assi, Varanasi, Uttar Pradesh, 221005',
-  github: 'https://github.com/AbhinavRai2004',
-  linkedIn: 'https://www.linkedin.com/in/abhinav2004',
-  twitter: 'https://twitter.com/AbhinavRai48039',
-  stackOverflow: 'https://stackoverflow.com/users/22012099/abhinav-rai',
+  profile: "/profile.png",
+  description:
+    "My name is Abhinav Rai, and I am a dedicated and enthusiastic programmer. I am a quick learner with a strong self-learning attitude, always eager to explore and master new technologies. I have a passion for problem-solving and a deep interest in all aspects of web application development. My core skill lies in ReactJs, NodeJs, JavaScript, and I enjoy leveraging it to create open and accessible web solutions. I am open to any job opportunities that align with my skills and interests.",
+  email: "raiabhinav.in@gmail.com",
+  phone: "+91-9335783068",
+  address: "Assi, Varanasi, Uttar Pradesh, 221005",
+  github: "https://github.com/AbhinavRai2004",
+  linkedIn: "https://www.linkedin.com/in/abhinav2004",
+  twitter: "https://twitter.com/AbhinavRai48039",
+  stackOverflow: "https://stackoverflow.com/users/22012099/abhinav-rai",
   leetcode: "https://leetcode.com/u/user400AB/",
-  resume: "https://drive.google.com/file/d/14FeRjlTgYUw7qeyxhf7X8zgPbC1Tf-Uz/view?usp=drive_link"
-}
+  resume:
+    "https://drive.google.com/file/d/1-IpHu9BZ6Pwr9s62Pc7va1Uygvf516u0/view?usp=drive_link"
+};
